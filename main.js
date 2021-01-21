fbc0364a7329f03b4ac2e03dc41184ed233d5461
Willmer Obed Fúneme San Martín <willmer.funeme@unac.edu.co>
2021-01-20 20:58:21 -0500

    variables_constantes_20_01_2021

---
+++ main.js
@@ -0,0 +1,27 @@
+var nombre = "Miguel";
+let edad = 23;
+// Var o Let: se utilizan para declarar las variables
+
+var profesor = true;
+let lista_cursos = ['JavaScript', 'PHP'];
+
+var informacion_personal = {
+    id: 10248658,
+    direccion: "Calle 11"
+};
+
+// Constante: contiene un dato estático que se almacena y no cambia
+
+console.log(nombre);
+console.log(edad);
+console.log(profesor);
+console.log(lista_cursos);
+console.log(informacion_personal);
+
+// variable tipo constante (declaración)
+
+const cedula = 123456789;
+
+console.warn(cedula);
+console.log(cedula);
+
